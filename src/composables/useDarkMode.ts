ccf8147bc66d48f77cef3325cbb86835c8d7c6c9
litvinPV <litvinpv@pba.su>
2025-04-14 16:14:37 +0300

    dynamic changed theme_color

--- src/composables/useDarkMode.ts
+++ src/composables/useDarkMode.ts
@@ -1,14 +1,27 @@
-import { ref, onMounted, onUnmounted } from "vue";
+import { ref, onMounted, onUnmounted, watch } from "vue";
 
 export function useDarkMode() {
   const isDark = ref<boolean>(false);
 
+  const updateThemeColor = () => {
+    const themeColor = isDark.value ? "#111827" : "#fafafa";
+    let metaThemeColor = document.querySelector("meta[name='theme-color']");
+    
+    if (!metaThemeColor) {
+      metaThemeColor = document.createElement("meta");
+      metaThemeColor.setAttribute("name", "theme-color");
+      document.head.appendChild(metaThemeColor);
+    }
+    
+    metaThemeColor.setAttribute("content", themeColor);
+  };
+
   const checkDarkMode = () => {
     isDark.value = document.documentElement.classList.contains("dark");
+    updateThemeColor();
   };
 
   const observer = ref<MutationObserver | null>(null);
-
   onMounted(() => {
     checkDarkMode();
     observer.value = new MutationObserver(checkDarkMode);
@@ -24,6 +37,9 @@ export function useDarkMode() {
       observer.value = null;
     }
   });
+
+  watch(isDark, updateThemeColor);
+
   return {
     isDark,
   };
